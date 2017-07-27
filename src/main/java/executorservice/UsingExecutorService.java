7132d533acea0e981324b3dc040951fa740db430
Charles Chong <charleshchong@gmail.com>
2017-07-27 00:14:52 -0500

    Clean pom.xml. Format code. Add future pkg.

--- src/main/java/executorservice/UsingExecutorService.java
+++ src/main/java/executorservice/UsingExecutorService.java
@@ -5,20 +5,20 @@ import java.util.concurrent.Executors;
 
 public class UsingExecutorService {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+	// TODO Auto-generated method stub
 
-		ExecutorService executorService = Executors.newFixedThreadPool(10);
-		executorService.execute(new Runnable() {
-			public void run(){
-				System.out.println("This thread is running - " + Thread.currentThread().getName());
-			}
-		});
-		
-		executorService.shutdown();
-	}
+	ExecutorService executorService = Executors.newFixedThreadPool(10);
+	executorService.execute(new Runnable() {
+	    public void run() {
+		System.out.println("This thread is running - " + Thread.currentThread().getName());
+	    }
+	});
+
+	executorService.shutdown();
+    }
 
 }
