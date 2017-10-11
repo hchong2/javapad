192c55796affb8f300e24ff498480b42354bd008
Charles Chong <charleshchong@gmail.com>
2017-10-11 01:37:37 -0500

    Update

--- src/main/java/finance/DataType.java
+++ src/main/java/finance/DataType.java
@@ -1,7 +1,5 @@
 package finance;
 
 public enum DataType {
-
     biggestGain, biggestLoss, cumulativeVolume, greenDays, redDays;
-
 }
--- src/main/java/httpclient/URIExamples.java
+++
@@ -1,8 +0,0 @@
-package httpclient;
-
-public class URIExamples {
-
-	public static void main(String [] args){
-		
-	}
-}
